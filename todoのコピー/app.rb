9636e7a869a91b9faba47dc9d99a79f955823668
Masaki Asanuma <50512727+masaasa-lit@users.noreply.github.com>
2019-11-26 18:03:45 +0900

    Add files via upload

---
+++ todoのコピー/app.rb
@@ -0,0 +1,85 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require 'sinatra/activerecord'
+require './models'
+
+enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
+
+before '/tasks' do
+  if current_user.nil?
+    redirect '/'
+  end
+end
+
+get '/' do
+  if current_user.nil?
+    @tasks = Task.none
+  else
+    @tasks = current_user.tasks
+  end
+  erb :index
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signup' do
+  user = User.create(
+    name: params[:name],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation]
+    )
+  if user.persisted?
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+get '/signin' do
+  erb :sign_in
+end
+
+post '/signin' do
+  user = User.find_by(name: params[:name])
+  if user && user.authenticate(params[:password])
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+get '/signout' do
+  session[:user] = nil
+  redirect '/'
+end
+
+get '/tasks/new' do
+  erb :new
+end
+
+post '/tasks' do
+  current_user.tasks.create(title: params[:title])
+  redirect '/'
+end
+
+post '/tasks/:id/done' do
+  task = Task.find(params[:id])
+  task.completed = true
+  task.save
+  redirect '/'
+end
+
+get '/tasks/:id/star' do
+  task = Task.find(params[:id])
+  task.star = !task.star
+  task.save
+  redirect '/'
+end
